d11f07462a7b86ed2d63bb36dce301baa8e20f38
Nima <smartnimaco@gmail.com>
2024-04-22 23:47:08 +0330

    Testing

--- customize.sh
+++ customize.sh
@@ -1,5 +1,56 @@
 #!/system/bin/sh
 
+chooseport_legacy() {
+  # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  [ "$1" ] && local delay=$1 || local delay=3
+  local error=false
+  while true; do
+    timeout 0 $MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32/keycheck
+    timeout $delay $MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32/keycheck
+    local sel=$?
+    if [ $sel -eq 42 ]; then
+      return 0
+    elif [ $sel -eq 41 ]; then
+      return 1
+    elif $error; then
+      abort "Volume key not detected!"
+    else
+      error=true
+      echo "Volume key not detected. Try again"
+    fi
+  done
+}
+
+chooseport() {
+  # Original idea by chainfire and ianmacd @xda-developers
+  [ "$1" ] && local delay=$1 || local delay=3
+  local error=false
+  while true; do
+    local count=0
+    while true; do
+      timeout $delay /system/bin/getevent -lqc 1 2>&1 > $TMPDIR/events &
+      sleep 0.5; count=$((count + 1))
+      if (`grep -q 'KEY_VOLUMEUP *DOWN' $TMPDIR/events`); then
+        return 0
+      elif (`grep -q 'KEY_VOLUMEDOWN *DOWN' $TMPDIR/events`); then
+        return 1
+      fi
+      [ $count -gt 6 ] && break
+    done
+    if $error; then
+      # abort "Volume key not detected!"
+      echo "Volume key not detected. Trying keycheck method"
+      export chooseport=chooseport_legacy VKSEL=chooseport_legacy
+      chooseport_legacy $delay
+      return $?
+    else
+      error=true
+      echo "Volume key not detected. Try again"
+    fi
+  done
+}
+
 debloat_list="$MODPATH/bloat_packagelist.txt"
 id=$(grep "id=" $MODPATH/module.prop | cut -d'=' -f2-)
 last_install="/data/adb/modules/$id"
@@ -11,6 +62,16 @@ else
   REPLACE=$(cat "$last_install/.lastreplace")
   REPLACE="$REPLACE "
   ui_print "- (Re)Installing... (This could take some time :D)"
+fi
+
+ui_print "| Do you want to install the default configuration [Press Vol+]"
+ui_print "| Or Customize your installation? [Press Vol-]"
+ui_print "| Waiting until a key is pressed..."
+
+if chooseport; then
+  CUSTOMIZE=false
+else
+  CUSTOMIZE=true
 fi
 
 # Loop through each line in the output of the one-liner
