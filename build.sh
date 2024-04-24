8c16c0cdca785af90c5caff26345ec39062e196b
Nima <smartnimaco@gmail.com>
2024-04-24 20:10:28 +0330

    Added LICENSE to ignore list

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ fi
 mod_id=$(grep "id=" module.prop | cut -d'=' -f2-)
 
 wget https://raw.githubusercontent.com/Systemless-DeBloaters/Framework/main/customize.sh
-zip -r $mod_id-$version.zip ./ -x .git/\* Screenshots/\* .github/\* README.md build.sh .gitignore
+zip -r $mod_id-$version.zip ./ -x .git/\* Screenshots/\* .github/\* README.md build.sh .gitignore LICENSE
 rm customize.sh
